d43836a29bd4cf9419913679b36fdeb0d006a456
nuno <nunomargsilva@gmail.com>
2026-03-15 10:37:21 -0300

    Movimentação do player pronta :D

--- Assets/player.cs
+++ Assets/player.cs
@@ -18,26 +18,35 @@ public class player : MonoBehaviour
     // Paredes
     public Transform leftWall;
     public Transform rightWall;
+    public Transform topWall;
+    public Transform bottomWall;
 
     void Update()
     {
         float movimento = 0f;
-
+        float movimentoVertical = 0f;
         if (Input.GetKey(moveLeft))
             movimento = -1f;
 
         if (Input.GetKey(moveRight))
             movimento = 1f;
-
+        if(Input.GetKey(moveUp))
+            movimentoVertical = 1f;
+        if(Input.GetKey(moveDown))
+            movimentoVertical = -1f; 
         // Move o jogador
         Vector3 deslocamento = Vector3.right * movimento * velocidade * Time.deltaTime;
+        Vector3 deslocamentoVert = Vector3.up * movimentoVertical * velocidade * Time.deltaTime;
+
         transform.Translate(deslocamento);
+        transform.Translate(deslocamentoVert);
 
         // Limites das paredes
-        if (leftWall != null && rightWall != null)
+        if (leftWall != null && rightWall != null && topWall != null && bottomWall != null)
         {
             Vector3 posicaoAtual = transform.position;
             posicaoAtual.x = Mathf.Clamp(posicaoAtual.x, leftWall.position.x, rightWall.position.x);
+            posicaoAtual.y = Mathf.Clamp(posicaoAtual.y, bottomWall.position.y, topWall.position.y);
             transform.position = posicaoAtual;
         }
 
